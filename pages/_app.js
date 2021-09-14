d179186e8dc5d6ec26f21bba5b8236ccfee980b3
john-raymon <john@johnraymon.com>
2021-09-14 02:39:12 -0400

    QA, clean up mobile

--- pages/_app.js
+++ pages/_app.js
@@ -5,15 +5,15 @@ import Image from 'next/image';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="relative w-full">
+    <div className="relative w-full flex-grow">
       <div className="absolute top-0 left-0 w-full">
           {/* <img href="/./../public/hero-lines.png" objectFit="cover"/> */}
           <img src="/hero-lines.png" className="object-cover w-full xl:max-h-[619px]" />
       </div>
-      <div className="font-base relative min-h-screen max-w-[77.87rem] w-full mx-auto sitems-center justify-center xl:px-0">
+      <div className="font-base relative min-h-screen lg:max-w-[77.87rem] w-full mx-auto justify-center xl:px-0">
         <header>
-          <div className="w-full my-16 px-10">
-            <div className="w-52 h-auto">
+          <div className="w-full my-16 px-8 lg:px-0">
+            <div className="w-28 lg:w-52 h-auto">
               <RunnerSvg className="fill-current text-runner-white"/>
             </div>
           </div>
