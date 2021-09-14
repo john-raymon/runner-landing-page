651cb295a94b3d0487dd9daeba9dd3ccdd966fbd
john-raymon <john@johnraymon.com>
2021-09-14 02:16:54 -0400

    QA changes - add responsiveness

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'lite-react-ui/css';
+import RunnerSvg from './../public/runner.svg';
+import Image from 'next/image';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="relative w-full">
+      <div className="absolute top-0 left-0 w-full">
+          {/* <img href="/./../public/hero-lines.png" objectFit="cover"/> */}
+          <img src="/hero-lines.png" className="object-cover w-full xl:max-h-[619px]" />
+      </div>
+      <div className="font-base relative min-h-screen max-w-[77.87rem] w-full mx-auto sitems-center justify-center xl:px-0">
+        <header>
+          <div className="w-full my-16 px-10">
+            <div className="w-52 h-auto">
+              <RunnerSvg className="fill-current text-runner-white"/>
+            </div>
+          </div>
+        </header>
+        <main className="w-full">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './public/**/*.html',
+    './pages/**/*.js',
+  ],
+  theme: {
+    fontFamily: {
+      base: 'Archivo',
+    },
+    extend: {
+      colors: {
+        'runner-white': '#FCEFED',
+        'runner-purple': '#6173F4',
+        'runner-dark-purple': '#3B2E40',
+        'runner-orange': '#F35E3E',
+        'runner-black': '#030408'
+      },
+      letterSpacing: {
+        widest: '.25em',
+        _widest: '0.3em',
+        __widest: '.5em',
+      },
+    }
+  },
+  variants: {
+    extend: {
+      display: ['hover', 'focus'],
+     },
+  }
+}
