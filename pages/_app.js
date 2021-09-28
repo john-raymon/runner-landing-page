dbc1ff444611e9f19b66c764c3f5fd6597b697b4
john-raymon <john@johnraymon.com>
2021-09-27 21:49:27 -0400

    Fix header padding

--- pages/_app.js
+++ pages/_app.js
@@ -10,22 +10,23 @@ function MyApp({ Component, pageProps }) {
       <div className="absolute md:h-auto top-0 left-0 w-full">
           <img src="/hero-lines.png" className="object-cover h-[43rem] opacity-80 w-full" />
       </div>
-      <div className="font-base relative min-h-screen lg:max-w-[77.87rem] w-full mx-auto justify-center px-8 xl:px-0">
-        <header className="px-2 py-10 mt-6 md:px-6 xl:px-24 sticky top-0 transparent-background-shadow z-20">
-          <div className="w-full flex justify-between items-center">
-            <Link href="/">
-              <a className="w-[7.25rem] lg:w-[12rem] h-auto">
-                <RunnerSvg className="fill-current text-runner-white"/>
-              </a>
-            </Link>
+      <header className="px-10 pb-10 pt-14 md:px-14 xl:px-24 sticky top-0 transparent-background-shadow z-20">
+        <div className="w-full flex justify-between items-center">
+          <Link href="/">
+            <a className="w-[7.25rem] lg:w-[12rem] h-auto">
+              <RunnerSvg className="fill-current text-runner-white"/>
+            </a>
+          </Link>
 
-            <Link href="/why">
-              <a className="text-runner-white text-sm tracking-[0.1em] text-[0.7rem] font-bold pb-[0.3rem]">
-                <span className="border-runner-purple border-b-[0.1rem] py-[0.3rem]">Why?</span>
-              </a>
-            </Link>
-          </div>
-        </header>
+          <Link href="/why">
+            <a className="text-runner-white text-sm tracking-[0.1em] text-[0.7rem] font-bold pb-[0.3rem]">
+              <span className="border-runner-purple border-b-[0.1rem] py-[0.3rem]">Why?</span>
+            </a>
+          </Link>
+        </div>
+      </header>
+      <div className="font-base relative min-h-screen lg:max-w-[77.87rem] w-full mx-auto justify-center px-8 xl:px-0">
+
         <main className="w-full">
           <Component {...pageProps} />
         </main>
